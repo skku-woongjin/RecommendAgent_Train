7a9815e30c3e3a9a694d7e4f49c215686888c8d7
Jaein Park <jaynap@g.skku.edu>
2022-07-18 17:03:39 +0900

    add

---
+++ Assets/showSphere.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showSphere : MonoBehaviour
+{
+    public GameObject Sphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Sphere.SetActive(false);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Sphere.SetActive(true);
+            Physics.IgnoreCollision(transform.parent.GetComponent<Collider>(), GameManager.Instance.owner.GetComponent<Collider>(), false);
+        }
+    }
+}
