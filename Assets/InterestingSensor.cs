bf9557dd5b3e26658eee24cc0e5b44513d9676de
Jaein Park <jaynap@g.skku.edu>
2022-07-22 10:30:45 +0900

    recov

--- Assets/InterestingSensor.cs
+++ Assets/InterestingSensor.cs
@@ -2,27 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class InterestingSensor : MonoBehaviour
+
 {
+
     public IdleAgent agent;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.tag == "target")
         {
-            agent.interest();
             agent.interestingObj = other.transform;
+            agent.interest();
         }
     }
 
