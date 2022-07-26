4061be3dfcbd9ee7f11de48a72322e39ce3694af
junwon25 <wwwnsdnjs@naver.com>
2022-07-26 14:15:01 +0900

    add  quest ui

--- Assets/Scripts/request.cs
+++ Assets/Scripts/request.cs
@@ -19,17 +19,21 @@ public class request : MonoBehaviour
     public TMP_Text catQuestText;
     public Button QuestBtn;
     public bool ishate = false;
+    private GameObject quest_panel;
     public string[] actionKeyword = {"jump","fly","walk","go","play","find"};
     public string[] locationKeyword = {"Jurassicpark","gamemachine","planetland","playground","castle"};
     void Start()
     {
         QuestBtn=GameObject.FindGameObjectWithTag("NewQuestBtn").GetComponent<Button>();
         QuestBtn.onClick.AddListener(NewQuest);
+        quest_panel = GameObject.Find("QuestPanel");
+        quest_panel.SetActive(false);
     }
     void NewQuest(){
         string location = locationKeyword[Random.Range(0,locationKeyword.Length)];
         string action = actionKeyword[Random.Range(0, actionKeyword.Length)];
         StartCoroutine(UploadKeyword("Let's*"+action+"*"+location));
+        quest_panel.SetActive(true);
     }
 
     // IEnumerator getRequest(string uri)
