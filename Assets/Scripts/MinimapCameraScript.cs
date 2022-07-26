45f49bb30dbf487e88ae2d8b664d0ad7afd225a1
junwon25 <wwwnsdnjs@naver.com>
2022-07-26 16:18:06 +0900

    add minimap animation

--- Assets/Scripts/MinimapCameraScript.cs
+++ Assets/Scripts/MinimapCameraScript.cs
@@ -10,22 +10,26 @@ public class MinimapCameraScript : MonoBehaviour
     private GameObject minimap_panel;
     private Button mapButton;
     private int checkMapOpen;
+    public Animator MapAnimator;
     void Start()
     {
-        checkMapOpen = 1;
+
+        checkMapOpen = 0;
         mapButton = GameObject.FindGameObjectWithTag("MapOpenBtn").GetComponent<Button>();
         mapButton.onClick.AddListener(mapOnOff);
         minimap_panel = GameObject.Find("MinimapPanel");
-        minimap_panel.SetActive(false);
+        minimap_panel.SetActive(true);
     }
     void mapOnOff(){
         if(checkMapOpen == 0){
+            MapAnimator.Play("MapAnimationOut");
             checkMapOpen = 1;
-            minimap_panel.SetActive(true);
+            //minimap_panel.SetActive(true);
         }
         else{
+            MapAnimator.Play("MapAnimation");
             checkMapOpen = 0;
-            minimap_panel.SetActive(false);
+            //minimap_panel.SetActive(false);
         }
     }
     // Update is called once per frame
